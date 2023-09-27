81287b430e823f30fef3099925202a37321ace5e
Maxim Gabidullin <after@ya.ru>
2023-09-27 19:17:20 +0300

    +get@block-controller

--- src/Controller/BlockController.php
+++ src/Controller/BlockController.php
@@ -2,12 +2,23 @@
 
 namespace App\Controller;
 
-use App\Repository\BlockRepository;
+use App\Entity\Block;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/private/')]
 class BlockController extends AbstractController
 {
+    #[Route('/private/blocks/{id}', methods: ['GET'])]
+    public function get(Block $block): JsonResponse
+    {
+        $block = [
+            'id' => $block->getId(),
+            'name' => $block->getName(),
+            'col' => $block->getCol(),
+            'sort' => $block->getSort(),
+            'private' => $block->isPrivate(),
+        ];
+        return $this->json($block);
+    }
 }
