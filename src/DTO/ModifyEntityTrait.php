1d3d500384ee5347d9af51b644da19f0c7830617
Maxim Gabidullin <after@ya.ru>
2023-09-28 19:29:49 +0300

    ~modify-entity-trait

--- src/DTO/ModifyEntityTrait.php
+++ src/DTO/ModifyEntityTrait.php
@@ -7,9 +7,6 @@ trait ModifyEntityTrait
     public function modifyEntity(object &$entity): void
     {
         foreach ($this as $field => $value) {
-            if ('boolean' === gettype($value) && null === $value) {
-                $value = false;
-            }
             $entity->{'set' . ucfirst($field)}($value);
         }
     }
