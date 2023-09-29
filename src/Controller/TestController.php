9d4a310c2b529053a2100f2de1761b45258a48c8
Maxim Gabidullin <after@ya.ru>
2023-09-29 10:11:58 +0300

    ~test-controller

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,19 +2,17 @@
 
 namespace App\Controller;
 
-use App\Repository\BlockRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends AbstractController
 {
-    #[Route('/private/test', name: 'app_private_test', methods: ['GET'])]
+    #[Route('/private/test', methods: ['GET'])]
     public function test(): JsonResponse
     {
         return $this->json([
-            'user' => $this->getUser()?->getUserIdentifier(),
+            $this->getUser()?->getUserIdentifier(),
         ]);
     }
 }
