145d5a6aa2d7fa010325cbc43f36adb2b1b8300a
Maxim Gabidullin <after@ya.ru>
2023-10-19 15:38:23 +0300

    -http-host-from-icons

--- src/Command/ModifyIconsCommand.php
+++ src/Command/ModifyIconsCommand.php
@@ -15,8 +15,8 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 final class ModifyIconsCommand extends Command
 {
-    final public const OLD_ICON_URL = 'https://icons.aftaa.ru/favicons/';
-    final public const NEW_ICON_URL = 'https://icons.aftaa.ru/';
+    final public const OLD_ICON_URL = 'https://icons.aftaa.ru/';
+    final public const NEW_ICON_URL = '';
 
     /**
      * @param LinkRepository $linkRepository
